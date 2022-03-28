a8abff79805c5e50143d6dd6a736c8d57a813ff1
Devin Lieuw A Soe <dlieuwasoe@gmail.com>
2022-03-28 16:35:23 +0200

    Added the *255

--- unprocessing/unprocess_images.py
+++ unprocessing/unprocess_images.py
@@ -1,25 +1,13 @@
-import cv2
 import unprocess
 import tensorflow as tf
-from plantcv import plantcv as pcv
 
 if __name__ == '__main__':
-    # Read RGB image
-    img = tf.io.read_file("data/r_0.png")
+    # img = tf.io.read_file("../nerf_synthetic/lego/train/r_0.png")
+    img = tf.io.read_file("test.png")
     png_file = tf.io.decode_png(img, channels=3)
     white_level = 255.0
     png_image = tf.cast(png_file, tf.float32) / white_level
-    # RGBimage = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # # Output img with window name as 'image'
-    # cv2.imshow('image', img)
-    raw_img, metadata= unprocess.unprocess(png_image)
-    tf.keras.utils.save_img(
-        'test.tiff', raw_img.numpy(), scale=False)
-    print(raw_img)
-    pcv.params.debug = "print"
-
-    # read in image
-    img, path, img_filename = pcv.readbayer("test.tiff")
-    # cv2.imshow('image2', raw_img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    raw_img, metadata = unprocess.unprocess(png_image)
+    raw_img_white_level = raw_img * 255.0
+    tf.keras.utils.save_img('raw_test.png', raw_img_white_level.numpy(), scale=False)
+    print(raw_img_white_level)
