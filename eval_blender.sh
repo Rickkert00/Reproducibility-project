c7980a4b9e2534ef9dc7430ced0ba005b93df055
Thomas Markhorst <t.c.markhorst@student.tudelft.nl>
2022-04-13 19:58:04 +0200

    Sequence run

--- eval_blender.sh
+++ eval_blender.sh
@@ -20,16 +20,55 @@
 #TRAIN_DIR=/Users/barron/tmp/nerf_results/$EXPERIMENT/$SCENE # weights/checkpoint
 #DATA_DIR=/Users/barron/data/nerf_synthetic/$SCENE #training
 
+
 SCENE=lego
 EXPERIMENT=debug
-FRAMES=60
-TRAIN_DIR=nerf_results/saved/all_results/nerf_raw_noise_60_1024
-DATA_DIR=nerf_results/saved/all_data_sets/nerf_raw_noise_60_train
+TRAIN_DIR1=nerf_results/saved/all_results/nerf_raw_noise_90_1024
+DATA_DIR1=nerf_results/saved/all_data_sets/ #nerf_raw_noise_60_train
 
 
 python3 -m eval \
-  --data_dir=$DATA_DIR \
-  --train_dir=$TRAIN_DIR \
+  --data_dir=$DATA_DIR1 \
+  --train_dir=$TRAIN_DIR1 \
+  --chunk=3076 \
+  --gin_file=configs/rawNERF.gin \
+  --logtostderr
+
+SCENE=lego
+EXPERIMENT=debug
+TRAIN_DIR2=nerf_results/saved/all_results/nerf_raw_noise_60_1024
+DATA_DIR2=nerf_results/saved/all_data_sets/ #nerf_raw_noise_60_train
+
+
+python3 -m eval \
+  --data_dir=$DATA_DIR2 \
+  --train_dir=$TRAIN_DIR2 \
+  --chunk=3076 \
+  --gin_file=configs/rawNERF.gin \
+  --logtostderr
+
+SCENE=lego
+EXPERIMENT=debug
+TRAIN_DIR3=nerf_results/saved/all_results/nerf_raw_noise_30_1024
+DATA_DIR3=nerf_results/saved/all_data_sets/ #nerf_raw_noise_60_train
+
+
+python3 -m eval \
+  --data_dir=$DATA_DIR3 \
+  --train_dir=$TRAIN_DIR3 \
+  --chunk=3076 \
+  --gin_file=configs/rawNERF.gin \
+  --logtostderr
+
+SCENE=lego
+EXPERIMENT=debug
+TRAIN_DIR4=nerf_results/saved/all_results/v2_nerf_raw_120_16384
+DATA_DIR4=nerf_results/saved/all_data_sets/ #nerf_raw_noise_60_train
+
+
+python3 -m eval \
+  --data_dir=$DATA_DIR4 \
+  --train_dir=$TRAIN_DIR4 \
   --chunk=3076 \
   --gin_file=configs/rawNERF.gin \
   --logtostderr
